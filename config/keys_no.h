a3b91329047859cecf817dc61c5900c11c752df2
y0m0 <daniele@byx.digital>
2025-06-26 17:23:42 +0200

    adds noarwegian chars to altgr

--- config/keys_no.h
+++ config/keys_no.h
@@ -5,6 +5,17 @@
 #include <dt-bindings/zmk/modifiers.h>
 #include <dt-bindings/zmk/keys.h>
 
-#define NO_AE    ALGR(SEMICOLON)           // Æ
-#define NO_OE    ALGR(SINGLE_QUOTE)        // Ø
-#define NO_AA    ALGR(BACKSLASH)           // Å
+#define NO_OE_ALT_CODE \
+    &macro_press &kp LALT \
+        &kp KP_0 &kp KP_2 &kp KP_1 &kp KP_6 \
+    &macro_release &kp LALT
+
+#define NO_AE_ALT_CODE \
+    &macro_press &kp LALT \
+        &kp KP_0 &kp KP_1 &kp KP_9 &kp KP_8 \
+    &macro_release &kp LALT
+
+#define NO_AA_ALT_CODE \
+    &macro_press &kp LALT \
+        &kp KP_0 &kp KP_1 &kp KP_9 &kp KP_7 \
+    &macro_release &kp LALT
